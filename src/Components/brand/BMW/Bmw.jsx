faefa4f54cd23d746bf7c74ff075ce84c6b2cfde
iftekharr001 <iftekharr001@gmail.com>
2023-10-20 11:43:20 -0700

    added swper slider in brand card

--- src/Components/brand/BMW/Bmw.jsx
+++ src/Components/brand/BMW/Bmw.jsx
@@ -1,10 +1,104 @@
 import { useLoaderData } from "react-router-dom";
+import { Link } from "react-router-dom";
 import BmwCardes from "./BmwCardes";
+import { Swiper, SwiperSlide } from "swiper/react";
 
+import "swiper/css";
+import "swiper/css/navigation";
+import { Navigation } from "swiper";
 const Bmw = () => {
   const bmwCard = useLoaderData();
   return (
     <>
+      <div>
+        <Swiper modules={[Navigation]} spaceBetween={50} slidesPerView={1} navigation >
+          <SwiperSlide>
+            <div
+              className="hero h-[90vh]"
+              style={{
+                backgroundImage:
+                  "url(https://i.ibb.co/Y08c1W6/pexels-mike-bird-170811.jpg)",
+              }}
+            >
+              <div className="hero-overlay bg-opacity-60 "></div>
+              <div className="hero-content text-left text-neutral-content ">
+                <div className="max-w-xl ">
+                  <h1 className="mb-5 text-5xl font-bold text-[#ff4605]">
+                    The Ultimate Driving Experience
+                  </h1>
+                  <p className="mb-5 text-white">
+                    BMW has redefined the art of driving. With a rich history of
+                    engineering excellence and a commitment to innovation
+                  </p>
+                  <Link to="/">
+                    <button className="btn bg-[#ff4605] hover:bg-[#81391e] text-white font-bold border-none">
+                      Visit Home
+                    </button>
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div
+              className="hero h-[90vh]"
+              style={{
+                backgroundImage:
+                  "url(https://i.ibb.co/NNbzj6R/pexels-maria-geller-2127039.jpg)",
+              }}
+            >
+              <div className="hero-overlay bg-opacity-60 "></div>
+              <div className="hero-content text-left text-neutral-content ">
+                <div className="max-w-xl ">
+                  <h1 className="mb-5 text-5xl font-bold text-[#ff4605]">
+                    Innovate the Future with BMW
+                  </h1>
+                  <p className="mb-5 text-white">
+                    BMW dedication to sustainable mobility is reshaping the
+                    future. With a growing lineup of electric and hybrid
+                    vehicles, BMW is at the forefront of eco-conscious driving
+                    without compromising the exhilaration.
+                  </p>
+                  <Link to="/">
+                    <button className="btn bg-[#ff4605] hover:bg-[#81391e] text-white font-bold border-none">
+                      Visit Home
+                    </button>
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div
+              className="hero h-[90vh]"
+              style={{
+                backgroundImage:
+                  "url(https://i.ibb.co/DRqQySG/pexels-dan-ro-14764071.jpg)",
+              }}
+            >
+              <div className="hero-overlay bg-opacity-60 "></div>
+              <div className="hero-content text-left text-neutral-content ">
+                <div className="max-w-xl ">
+                  <h1 className="mb-5 text-5xl font-bold text-[#ff4605]">
+                    Elegance Meets Performance
+                  </h1>
+                  <p className="mb-5 text-white">
+                    BMW is synonymous with a perfect fusion of elegance and
+                    high-performance engineering. Each BMW vehicle embodies the
+                    brand legacy of luxury
+                  </p>
+                  <Link to="/">
+                    <button className="btn bg-[#ff4605] hover:bg-[#81391e] text-white font-bold border-none">
+                      Visit Home
+                    </button>
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </SwiperSlide>
+          ...
+        </Swiper>
+      </div>
       <div className="grid grid-cols-3 gap-6 w-[1140px] mx-auto">
         {bmwCard
           .filter((bmw) => bmw.carbrand === "bmw")
--- src/Components/brand/Toyota/Toyota.jsx
+++ src/Components/brand/Toyota/Toyota.jsx
@@ -1,6 +1,8 @@
 import { useLoaderData } from "react-router-dom";
 import ToyotaCardes from "./ToyotaCardes";
-
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import { Link } from "react-router-dom";
 
 
 const Toyota = () => {
@@ -10,6 +12,99 @@ const Toyota = () => {
 
   return (
     <>
+      <div>
+        <Swiper spaceBetween={50} slidesPerView={1}>
+          <SwiperSlide>
+            <div
+              className="hero h-[90vh]"
+              style={{
+                backgroundImage:
+                  "url(https://i.ibb.co/x7s164S/pexels-erik-mclean-17507722.jpg)",
+              }}
+            >
+              <div className="hero-overlay bg-opacity-60 "></div>
+              <div className="hero-content text-left text-neutral-content ">
+                <div className="max-w-xl ">
+                  <h1 className="mb-5 text-5xl font-bold text-[#ff4605]">
+                    {" "}
+                    Driving Innovation
+                  </h1>
+                  <p className="mb-5 text-white">
+                    Explore Toyota, a brand synonymous with groundbreaking
+                    technology and sustainable mobility.
+                  </p>
+                  <Link to="/">
+                    <button className="btn bg-[#ff4605] hover:bg-[#81391e] text-white font-bold border-none">
+                      Visit Home
+                    </button>
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div
+              className="hero h-[90vh]"
+              style={{
+                backgroundImage:
+                  "url(https://i.ibb.co/NSSzxXH/pexels-garvin-st-villier-3874330.jpg)",
+              }}
+            >
+              <div className="hero-overlay bg-opacity-60 "></div>
+              <div className="hero-content text-left text-neutral-content ">
+                <div className="max-w-xl ">
+                  <h1 className="mb-5 text-5xl font-bold text-[#ff4605]">
+                    {" "}
+                    Toyota Epitome of Reliability
+                  </h1>
+                  <p className="mb-5 text-white">
+                    Discover the Toyota Camry, an enduring symbol of dependable
+                    performance and elegance. Known for its durability and
+                    timeless design, the Camry is a testament to Toyotas
+                    commitment to quality
+                  </p>
+                  <Link to="/">
+                    <button className="btn bg-[#ff4605] hover:bg-[#81391e] text-white font-bold border-none">
+                      Visit Home
+                    </button>
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </SwiperSlide>
+          <SwiperSlide>
+            <div
+              className="hero h-[90vh]"
+              style={{
+                backgroundImage:
+                  "url(https://i.ibb.co/9V5MY7z/pexels-garvin-st-villier-3874337.jpg)",
+              }}
+            >
+              <div className="hero-overlay bg-opacity-60 "></div>
+              <div className="hero-content text-left text-neutral-content ">
+                <div className="max-w-xl ">
+                  <h1 className="mb-5 text-5xl font-bold text-[#ff4605]">
+                    {" "}
+                    Toyota Prius Pioneering Hybrid Excellence
+                  </h1>
+                  <p className="mb-5 text-white">
+                    Experience the Toyota Prius, the trailblazer of hybrid
+                    technology. With a commitment to reducing emissions and
+                    increasing efficiency, the Prius redefines eco-friendly
+                    driving without sacrificing style and comfort.
+                  </p>
+                  <Link to="/">
+                    <button className="btn bg-[#ff4605] hover:bg-[#81391e] text-white font-bold border-none">
+                      Visit Home
+                    </button>
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </SwiperSlide>
+          ...
+        </Swiper>
+      </div>
       <div className="grid grid-cols-3 gap-6 w-[1140px] mx-auto">
         {toyotaCard
           .filter((toyota) => toyota.carbrand === "toyota")
